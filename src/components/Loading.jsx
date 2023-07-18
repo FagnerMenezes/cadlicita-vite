4a6a5c6bedc37430035c7ae76d549ea83151fec2
Fagner Menezes <fsantosmenezes@gmail.com>
2023-07-17 22:05:49 -0300

    update

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,8 @@
 export default function Spinner() {
   return (
     <div className="flex flex-col justify-center items-center w- h-full gap-3">
-      <div className=" inline-block h-24 w-24 animate-spin rounded-full border-4 border-blue-300 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
-      <p className="text-cyan-700">Carregando...</p>
+      <div className=" inline-block h-24 w-24 animate-spin rounded-full border-4 border-cyan-700 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
+      <p className="text-cyan-700 uppercase font-bold">Carregando</p>
     </div>
   );
 }
--- src/pages/biddings/dataTable/Rows.jsx
+++ src/pages/biddings/dataTable/Rows.jsx
@@ -9,19 +9,29 @@ const Rows = (dataGovernment) => {
       status: { name: statusName },
     } = item.process_data;
 
-    const [{ _id, name: govermentName }] = item.government.filter((org) =>
+    const [{ _id, name: governmentName }] = item.government.filter((org) =>
       org.manager.includes("true")
     );
     return {
       edital: bidding_notice,
-      orgao: govermentName,
+      orgao: governmentName,
       data_final: new Date(date_finish).toLocaleDateString(),
       portal: item.process_data.portal.name,
       status: Status(statusName),
-      //TODO create className tailwindcss for buttons delete
-      e: <FaTrash className="text-red-500 cursor-pointe" />,
-      //TODO create className tailwindcss for buttons edit
-      d: <FaEdit className="text-cyan-600 cursor-pointer" />,
+      e: (
+        <FaTrash
+          className="text-red-500 "
+          data-te-toggle="tooltip"
+          title={"excluir"}
+        />
+      ),
+      d: (
+        <FaEdit
+          className="text-cyan-600 cursor-pointer"
+          title={"editar"}
+          data-te-toggle="tooltip"
+        />
+      ),
       s: statusName,
       id: _id,
     };
