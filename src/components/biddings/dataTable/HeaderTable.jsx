a1d7d5bc85ba8507cd348433dde084f222627b7d
Fagner Menezes <fsantosmenezes@gmail.com>
2023-07-21 21:59:15 -0300

    update branch create modal

--- src/components/biddings/dataTable/HeaderTable.jsx
+++ src/components/biddings/dataTable/HeaderTable.jsx
@@ -1,16 +1,27 @@
 import PropTypes from "prop-types";
-import Filter from "@/components/biddings/dataTable/Filter";
+import { FaFilter, FaSearch } from "react-icons/fa";
 
 export default function HeaderTable({ title, text, OnclickSearch, OnKeyDown }) {
   return (
     <div className="w-full flex flex-wrap items-center p-2 ">
       <h1 className=" w-full text-2xl text-gray-500 font-semibold">{title}</h1>
       <div className="flex justify-end items-center w-full g-2">
-        <Filter
-          text={text}
-          OnKeyDown={OnKeyDown}
-          OnclickFilter={OnclickSearch}
-        />
+        <div className="flex w-auto justify-center border border-r-transparent rounded-md rounded-r-none h-8 bg-white p-2 gap-2 hover:border-indigo-400  ">
+          <FaSearch className="text-gray-400" />
+          <input
+            type="search"
+            className="outline-none h-5"
+            value={text}
+            onKeyDown={OnKeyDown}
+          />
+        </div>
+        <button
+          className="group border h-8 hover:bg-blue-700 hover:text-white outline-indigo-400"
+          title={"Localizar"}
+          onClick={OnclickSearch}
+        >
+          <FaFilter className="text-blue-600 m-1 hover:text-white group" />
+        </button>
       </div>
     </div>
   );
